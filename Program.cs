e5f9bec11541e5639cfef7f20c49446139692d7b
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2023-07-26 00:23:39 -0400

    Updated main loop to async and determined that unit will send a response after getting a 4kb packet sent to it

--- Program.cs
+++ Program.cs
@@ -11,108 +11,203 @@ using System.Threading;
 
 namespace BoschCFW
 {
-    class Program
+  class Program
     {
-        public static UsbEndpointWriter Writer;
-        public static UsbEndpointReader Reader;
-        static UsbDevice Bike = null;
+        public static DateTime LastDataEventDate = DateTime.Now;
+        public static UsbDevice MyUsbDevice;
 
-        static void Main()
+        #region SET YOUR USB Vendor and Product ID!
+
+        public static UsbDeviceFinder MyUsbFinder;
+
+        #endregion
+
+        public static void Main(string[] args)
         {
+            ErrorCode ec = ErrorCode.None;
 
-
-            UsbRegDeviceList Devices = null;
-
-            while (Bike == null)
+            try
             {
-                while (Devices == null || Devices?.Count == 0)
+                // Find and open the usb device.
+                var BoschDevice = UsbDevice.AllDevices.Where(i => i.Name.Contains("Bosch")).First();
+                MyUsbFinder = new UsbDeviceFinder(BoschDevice.Vid, BoschDevice.Pid);
+
+                 BoschDevice.Open(out MyUsbDevice);
+
+                // If the device is open and ready
+                if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+
+
+                // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                // 'wholeUsbDevice' variable will be null indicating this is 
+                // an interface of a device; it does not require or support 
+                // configuration and interface selection.
+                IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
                 {
-                    Devices = UsbDevice.AllDevices;
-                    Thread.Sleep(1000);
+                    // This is a "whole" USB device. Before it can be used, 
+                    // the desired configuration and interface must be selected.
+
+                    // Select config #1
+                    wholeUsbDevice.SetConfiguration(1);
+
+                    // Claim interface #0.
+                    wholeUsbDevice.ClaimInterface(0);
                 }
 
-                foreach (var Device in Devices.ToList())
+                // open read endpoint 1.
+                UsbEndpointReader reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+
+                // open write endpoint 1.
+                UsbEndpointWriter writer = MyUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
+
+                // Remove the exepath/startup filename text from the begining of the CommandLine.
+                string cmdLine = Regex.Replace(
+                    Environment.CommandLine, "^\".+?\"^.*? |^.*? ", "", RegexOptions.Singleline);
+
+                if (!String.IsNullOrEmpty(cmdLine))
                 {
-                    bool Output;
-                    try { 
-                       Output = UsbDevice.OpenUsbDevice(ref Device.DeviceInterfaceGuids[0], out Bike);
+                    reader.DataReceived += (OnRxEndPointData);
+                    reader.DataReceivedEnabled = true;
+
+
+
+
+
+                    ////Brute force loop
+                    //int maxLength = 5; // Maximum length of the string to brute force
+                    //char[] charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+                    //// Add any other characters you want to include in the charset
+
+                    //// Loop through all possible string combinations
+                    //for (int length = 1; length <= maxLength; length++)
+                    //{
+                    //    char[] currentString = new char[length];
+
+                    //    // Initialize the currentString with the first character in the charset
+                    //    for (int i = 0; i < length; i++)
+                    //    {
+                    //        currentString[i] = charset[0];
+                    //    }
+
+                    //    while (true)
+                    //    {
+                    //        // Process the currentString (e.g., print, compare, etc.)
+                    //        string resultString = new string(currentString);
+                    //        Console.WriteLine(resultString);
+
+
+
+                    //        int bytesWritten;
+                    //        ec = writer.Write(Encoding.Default.GetBytes(resultString), 2000, out bytesWritten);
+                    //        if (ec != ErrorCode.None) throw new Exception(UsbDevice.LastErrorString);
+
+                    //        LastDataEventDate = DateTime.Now;
+                    //        while ((DateTime.Now - LastDataEventDate).TotalMilliseconds < 100)
+                    //        {
+                    //        }
+
+                    //        // Increment the currentString to the next combination
+                    //        int index = length - 1;
+                    //        while (index >= 0 && currentString[index] == charset[charset.Length - 1])
+                    //        {
+                    //            currentString[index] = charset[0];
+                    //            index--;
+                    //        }
+
+                    //        if (index < 0)
+                    //        {
+                    //            // The loop has reached the last combination for the current length
+                    //            break;
+                    //        }
+
+                    //        // Move to the next combination
+                    //        currentString[index] = charset[Array.IndexOf(charset, currentString[index]) + 1];
+                    //    }
+                    //}
+
+                    while (true)
+                    {
+                        // Size of the packet in bytes (4KB)
+                        int packetSize = 4096;
+
+                        // Create a byte array to hold the packet data
+                        byte[] packet = new byte[packetSize];
+
+                        // Fill the packet array with sample data (you can replace this with your actual data)
+                        for (int i = 0; i < packetSize; i++)
+                        {
+                            // Filling the packet with some arbitrary data (e.g., incrementing values)
+                            packet[i] = (byte)(i % 256); // Modulo 256 to ensure values are within 0-255 range
+                        }
+
+
+                        int bytesWritten;
+                        writer.Write(packet, 2000, out bytesWritten);
+
+
+
+                        //ec = writer.Write(Encoding.Default.GetBytes(Console.ReadLine()), 2000, out bytesWritten);
+                        if (ec != ErrorCode.None) throw new Exception(UsbDevice.LastErrorString);
                     }
-                    catch { }
-                    if (Bike != null) { break; }
+
+
+
+
+
+
+
+
+
+                    // Always disable and unhook event when done.
+                    reader.DataReceivedEnabled = false;
+                    reader.DataReceived -= (OnRxEndPointData);
+
+                    Console.WriteLine("\r\nDone!\r\n");
                 }
-                Thread.Sleep(100);
+                else
+                    throw new Exception("Nothing to do.");
             }
-
-            
-
-
-
-            //Determined from USBView https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/usbview?redirectedfrom=MSDN
-            //idVendor: 0x108C = Robert Bosch GmbH
-            //idProduct: 0x0182
-            //iSerialNumber: 0x03
-            //English(United States)  "953021702D3"
-
-            //var Serial = new UsbDeviceFinder("953021702D3");
-            //var PIDVID = new UsbDeviceFinder(4236, 386);
-            //UsbDevice Bike = UsbDevice.OpenUsbDevice(PIDVID);
-
-            Bike.Open();
-            
-
-            Writer = Bike.OpenEndpointWriter(WriteEndpointID.Ep01);
-            Reader = Bike.OpenEndpointReader(ReadEndpointID.Ep01);
-            Reader.DataReceivedEnabled = true;
-
-
-            //Writer.Abort();
-            //Writer.Reset();
-            //Writer.Flush();
-            //Reader.Abort();
-            //Reader.ReadFlush();
-            //Reader.Flush();
-            //Reader.Reset();
-            
-
-            string Response = null;
-
-            while (Response == null)
+            catch (Exception ex)
             {
-
-                for (char c = char.MinValue; c <= char.MaxValue; c++)
-                {
-                    Response = Write(c.ToString());                    
-                }
-                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
             }
-            Debug.WriteLine(Response);
+            finally
+            {
+                if (MyUsbDevice != null)
+                {
+                    if (MyUsbDevice.IsOpen)
+                    {
+                        // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                        // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                        // 'wholeUsbDevice' variable will be null indicating this is 
+                        // an interface of a device; it does not require or support 
+                        // configuration and interface selection.
+                        IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                        if (!ReferenceEquals(wholeUsbDevice, null))
+                        {
+                            // Release interface #0.
+                            wholeUsbDevice.ReleaseInterface(0);
+                        }
+                        MyUsbDevice.Close();
+                    }
+                }
+                MyUsbDevice = null;
 
+                // Free usb resources
+                UsbDevice.Exit();
+
+                // Wait for user input..
+                Console.ReadKey();
+            }
         }
 
-
-        static string Write(string Command)
+        private static void OnRxEndPointData(object sender, EndpointDataEventArgs e)
         {
-            Command.Replace("\r", "");
-            byte[] BytesToWrite = ASCIIEncoding.ASCII.GetBytes(Command + "\r");
-
-            var Error = Writer.Write(BytesToWrite, 3000, out _);
-            Debug.WriteLineIf(Error != 0, Error.ToString());
-
-            int ByteCount;
-            byte[] Buffer = new byte[64];
-
-            Error = Reader.Read(Buffer, 5, out ByteCount);
-            Debug.WriteLineIf(Error != 0, Error.ToString());
-
-            Buffer = Buffer.Take(ByteCount).ToArray();
-
-            var Output = Encoding.ASCII.GetString(Buffer);
-
-            Output = Output.Replace("\0", "");
-            Output = Output.Replace("\r", "");
-
-            return Output;
-
+            LastDataEventDate = DateTime.Now;
+            Console.Write(Encoding.Default.GetString(e.Buffer, 0, e.Count));
         }
     }
 }
